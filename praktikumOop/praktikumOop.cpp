3eb6b28724caf699bc66095b634d103424de5712
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-27 09:58:08 +0700

    pembuatan class seseorang

--- praktikumOop/praktikumOop.cpp
+++ praktikumOop/praktikumOop.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 using namespace std;
 
+class seseoramg {
+public: 
+	virtual void pesan() {
+		cout << "pesan dari seseorang" << endl;
+	}
+	//virtual void pesan() = 0;
+};
+
 int main()
 {
    
