76626272338cd4484e77bf64e04930efc260710a
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-27 10:37:26 +0700

    pembuatan class abstraksi dan menggunakan header set XY

---
+++ abstraksi/abstraksi.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+class abstraksi {
+private:
+    string x, y;
+
+public:
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+
+};
+
+int main()
+{
+
+}
+
